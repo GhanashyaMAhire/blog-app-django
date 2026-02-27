f0bb84b0896b9f12102e2bf13f5e181f61ae472d
GhanashyaMAhire <ghanshamahire.5558@gmail.com>
2026-02-27 15:54:37 +0530

    post by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -7,12 +7,11 @@ from blogs.models import Blog, Category
 
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status="Published")
     posts = Blog.objects.filter(is_featured=False, status="Published")
 
     context = {
-        'categories': categories,
+
         'featured_posts':featured_posts,
         'posts':posts,
     }
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from blogs.models import Blog, Category
 
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    # fetch the post belongs to category_id
+    posts = Blog.objects.filter(status="Published", category=category_id)
+    '''
+    try:
+        category = Category.objects.get(pk=category_id)  # pk is important here.
+    except:
+        # redirect users to home page.
+        return redirect('home')
+    '''
+    
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts':posts,
+        'category':category,
+    }
+    return render(request, 'posts_by_category.html', context)
